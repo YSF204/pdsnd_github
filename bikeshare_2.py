d5c7087ee5112587ee9cb3568ad8a68eb627e5aa
Yousef AL-Bakri <yousef204b@gmail.com>
2025-06-09 21:50:16 +0300

    Added welcoming messege to the program

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -23,6 +23,7 @@ def get_filters():
 
     # get user input for day of week (all, monday, tuesday, ... sunday)
 
+    print('*'*50 + '\nWelcome to the Bikeshare Analysis Program!\n' + '*'*50)
 
     print('-'*40)
     return city, month, day
